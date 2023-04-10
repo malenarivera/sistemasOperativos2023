9de5ee557666f9c476f9cab6571d3fd1ef41a966
malena <89429614+malenarivera@users.noreply.github.com>
2023-04-10 20:21:51 -0300

    Add files via upload

--- planificadorDeProcesos.c
+++ planificadorDeProcesos.c
@@ -12,64 +12,26 @@ int pid_c, int ms_c /* PID de c y rafaga de c en ms */
 	int pidMain= getpid();
 	/*obtener la prioridad del planificador (proceso con la mas alta prioridad) */
 	int prioridadMain= getprio(pidMain);
-
+	/*Pongo los pids en un arreglo*/
+	int pids[] = {pid_a,pid_b,pid_c};
+	int tiempoDispo[] = {ms_a,ms_b,ms_c};
+	int i = 0;
 	while (1) { 
-	    /* obtener la prioridad del proceso a */
-			int prioA= getprio(pid_a);
+	    int pidCambiar = pids[i%3]; 
+	    /* obtener la prioridad del proceso a cambiar */
+			int prioCambiar= getprio(pidCambiar);
 
-		/* cambiar la prioridad del proceso a, a un valor igual a la prioridad del planificador menos 1 */
-			chprio(pid_a, prioridadMain-1);
-
-			
-
-		/* liberar la CPU por ms_a ms (ponerse a dormir), por lo que Xinu asignará la CPU al proceso a */
-			sleepms(ms_a);
-
-		/* devolverle al proceso a su prioridad original */
-			chprio(pid_a, prioA);
-
-			
+		/* cambiar la prioridad del proceso, a un valor igual a la prioridad del planificador menos 1 */
+			chprio(pidCambiar, prioridadMain-1);
 
 
-/* ---------------------------- */
-/* planificar aquí el proceso b */
-/* ---------------------------- */
-
-        /* obtener la prioridad del proceso b */
-			int prioB= getprio(pid_b);
-
-		/* cambiar la prioridad del proceso b, a un valor igual a la prioridad del planificador menos 1 */
-			chprio(pid_b, prioridadMain-1);
-
-			
-
-		/* liberar la CPU por ms_a ms (ponerse a dormir), por lo que Xinu asignará la CPU al proceso b */
-			sleepms(ms_b);
-
-		/* devolverle al proceso b su prioridad original */
-			chprio(pid_b, prioB);
-
-			
-
-/* ---------------------------- */
-/* planificar aquí el proceso c */
-/* ---------------------------- */
-
-        /* obtener la prioridad del proceso c */
-			int prioC= getprio(pid_c);
-
-		/* cambiar la prioridad del proceso c, a un valor igual a la prioridad del planificador menos 1 */
-			chprio(pid_c, prioridadMain-1);
-
-		
-
-		/* liberar la CPU por ms_a ms (ponerse a dormir), por lo que Xinu asignará la CPU al proceso c*/
-			sleepms(ms_c);
+		/* liberar la CPU por ms_a ms (ponerse a dormir), por lo que Xinu asignará la CPU al proceso*/
+			sleepms(tiempoDispo[i%3]);
 
 		/* devolverle al proceso a su prioridad original */
-			chprio(pid_c, prioC);
-
-			
+			chprio(pidCambiar, prioCambiar);
+        
+        i++;
 	  }   
 }
 
@@ -81,17 +43,16 @@ int x=0;
 	}
 }
 
-
-
-void inicializarProcesitos(void){
+process organizador(){
 int pidA, msA, pidB, msB, pidC, msC;
-pidA=create(mensajito, 1024, 20, "process 2", 1,'A');
+
+pidA=create(mensajito, 1024, 20, "Process A", 1,'A');
 pidB=create(mensajito,1024, 20, "Process B", 1,'B');
 pidC=create(mensajito,1024, 20, "Process C", 1,'C');
 
-msA= ((60*200)/100);
-msB= ((30*200)/100);
-msC= ((10*200)/100);
+msA= ((60*200)/100);/*Calcula el 60%*/
+msB= ((30*200)/100);/*Calcula el 30%*/
+msC= ((10*200)/100);/*Calcula el 10%*/
 
 resume(pidA);
 resume(pidB);
@@ -100,9 +61,10 @@ resume(pidC);
 cambiandoPrioridades(pidA,msA,pidB,msB,pidC,msC);
 }
 
-void planificadorDeProcesos(){
-int pidProcesito;
-pidProcesito=create (inicializarProcesitos,1024,30,"procesito",0);
-resume(pidProcesito);
+
+process planificadorDeProcesos(){
+    int pidsh = create(organizador,1024,30,"Proceso Organizador",0);
+    resume(pidsh);
 }
+
 
